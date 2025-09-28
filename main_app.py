b643d24c0348e543b3c9b1052de9bdd1f96f5480
cho46 <chonhy1@naver.com>
2025-09-28 16:04:50 +0900

    250928

--- main_app.py
+++ main_app.py
@@ -201,4 +201,4 @@ def log_user_visit(response):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0',port=5000)
